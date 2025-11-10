0c4f6cc8b1a1afe50c5f590efd08f6204f97c1f2
edensuh1-art <edensuh1@gmail.com>
2025-11-10 16:36:01 -0600

    Add files via upload

---
+++ Team-Eve-Tournament-Management-main/src/main/java/edu/augustana/csc305/project/Organizer.java
@@ -0,0 +1,10 @@
+package edu.augustana.csc305.project;
+public class Organizer extends User{
+	private String name;
+	private String username;
+	//private final String password;
+
+	public Organizer(String username, String password){
+		super(username, password);
+	}
+}
